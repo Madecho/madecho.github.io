f23480a393d373865c08a5392d2ca3349c869098
Decho <madecho97@gmail.com>
2026-02-18 15:06:11 +0800

    revise

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,4 +3,13 @@ module.exports = {
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
+  // Keep layout and content classes so main page content is never clipped
+  safelist: {
+    standard: [
+      "post", "clearfix", "post-header", "post-title", "desc",
+      "container", "mt-5", "row", "col-sm-9", "col-sm-3",
+      "profile", "float-right", "float-left", "more-info", "profile-social",
+      "site-name-first", "site-name-rest"
+    ],
+  },
 };
